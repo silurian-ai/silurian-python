7b65cafc0ec056cecf31340093ca5d3901786d38
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-06 01:23:29 +0000

    Release 0.0.11

--- src/silurian/core/client_wrapper.py
+++ src/silurian/core/client_wrapper.py
@@ -14,10 +14,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "silurian/0.0.10",
+            "User-Agent": "silurian/0.0.11",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "silurian",
-            "X-Fern-SDK-Version": "0.0.10",
+            "X-Fern-SDK-Version": "0.0.11",
         }
         headers["X-API-KEY"] = self.api_key
         return headers
